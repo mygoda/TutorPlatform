ad4ebebfdad55f030168866f495e6487812eed3d
guwenxin <wenxin.gu@capitalonline.net>
2018-03-08 21:37:04 +0800

    更改镜像地址

--- bin/start_test.sh
+++ bin/start_test.sh
@@ -1,6 +1,6 @@
 
 echo "docker-compose down"
-/usr/local/bin/docker-compose -f docker-compose_test.yml down
+/usr/local/bin/docker-compose -f docker-compose-test.yml down
 
 echo "docker-compose up"
-/usr/local/bin/docker-compose -f docker-compose_test.yml up -d
+/usr/local/bin/docker-compose -f docker-compose-test.yml up -d
